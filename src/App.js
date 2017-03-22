75fbf232b9594c135bd1a7f35df7bd2ed6b0f8cf
welinton <willienn@hotmail.com>
2017-03-22 17:28:28 -0500

    Se corregi funcionalidad

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
 	constructor(){
 
 	super();
-	this.state={mensaje: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	this.state={
 
 		       terms: false}
 
@@ -16,7 +16,7 @@ class App extends Component {
     return (
       <div className="wrapper">
         <label><input type="checkbox"  checked={this.state.terms} onClick={this.update.bind(this)}/> Mostrar información importante</label>
-        {this.state.terms===true ? <p>{this.state.mensaje}</p>:null}
+        {this.state.terms===true ? <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>:null}
       </div>
     );
   }
