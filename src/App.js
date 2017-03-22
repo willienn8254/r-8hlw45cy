c8ca9c775c0c222dd4c4719c2c4991fe84cb75d8
welinton <willienn@hotmail.com>
2017-03-22 16:42:32 -0500

    Se agrega funcionalidad Mostrar mensaje

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+	constructor(){
+
+	super();
+	this.state={mensaje: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+		       terms: false}
+
+
+}
   render() {
     return (
       <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <label><input type="checkbox"  checked={this.state.terms} onClick={this.update.bind(this)}/> Mostrar información importante</label>
+        {this.state.terms===true ? <p>{this.state.mensaje}</p>:null}
       </div>
     );
+  }
+
+  update(event){
+
+
+  	this.setState({
+
+  		terms: event.target.checked
+  	})
   }
 }
 
