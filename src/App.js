1d76aaccc41bc31084514a799aecc0b305dc630f
welinton <willienn@hotmail.com>
2017-03-27 20:49:36 -0500

    Se modifica metodo Update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     return (
       <div className="wrapper">
         <label><input type="checkbox"  checked={this.state.terms} onClick={this.update.bind(this)}/> Mostrar información importante</label>
-        {this.state.terms===true ? <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>:null}
+        {this.state.terms==true ? <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>:null}
       </div>
     );
   }
@@ -26,7 +26,7 @@ class App extends Component {
 
   	this.setState({
 
-  		terms: event.target.checked
+  		terms: !this.state.terms
   	})
   }
 }
